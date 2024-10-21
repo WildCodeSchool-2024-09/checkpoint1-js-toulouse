5b6fe544ae86292eddfce8eba5f34a69a331487f
Hakkenheiner <remi.zicken@gmail.com>
2024-10-21 11:26:46 +0200

    finish Javascript step 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,13 @@
 const avatar = document.querySelector('.avatar')
+const modifyButton = document.querySelector('.modify-button')
 
 avatar.addEventListener('click', () => {
     avatar.src = './image/avatar.svg'
+})
+
+modifyButton.addEventListener('click', () => {
+    const name = prompt('Enter yout name :')
+    const firstName = document.querySelector('#firstname')
+    firstName.textContent = name
+    firstName.style.color = 'white'
 })
