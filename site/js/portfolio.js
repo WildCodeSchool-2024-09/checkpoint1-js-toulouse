3ea41b7fd01b179f90a08f2ad765518484ea239c
ArthurHtbk <arthur.heurtebise@gmail.com>
2024-10-21 16:00:33 +0200

    js step2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,14 @@
 const avatar = document.querySelector(".hero > img")
+const colorThingy = document.querySelector(".color-thingy")
+const firstname = document.querySelector("#firstname")
 
 avatar.addEventListener("click", () => {
     avatar.src = "./image/avatar.svg"
+})
+
+colorThingy.addEventListener("click", () => {
+    const text = prompt("Enter text")
+
+    firstname.textContent = text
+    firstname.style.color = "white"
 })
