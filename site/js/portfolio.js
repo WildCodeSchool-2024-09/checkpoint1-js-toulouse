b56c6dcf4d580ea221951dcf22a3427e165902bd
Gamberge <cedric.righi@gmail.com>
2024-10-21 11:29:15 +0200

    finish all

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,19 +1,47 @@
 const avatarImg = document.querySelector(".avatar-img");
-const modifyButton = document.querySelector(".modify-button");
+const modifyPageButton = document.querySelector(".modify-page-button");
 const firstname = document.getElementById("firstname");
 const description = document.querySelector(".description");
-const pinkBg = document.querySelector(".pink-bg");
-const pinkText = document.querySelector(".pink-text");
+const pinkBg = document.querySelectorAll(".pink-bg");
+const pinkText = document.querySelectorAll(".pink-text");
+const links = document.querySelectorAll(".links");
+const modifyListButton = document.querySelector(".modify-list-button");
+const LiFrontDevTools = document.querySelectorAll(".li-front-dev-tools");
+const modifyAddButton = document.querySelector(".modify-add-button");
+const inputBack = document.getElementById("input-back-dev-tools");
+const ulBackTools = document.getElementById("back-dev-tools-list");
 
 avatarImg.addEventListener("click", () => {
   avatarImg.src = "image/avatar.svg";
 })
 
-modifyButton.addEventListener("click", () => {
-  const bcgColor = prompt("Enter a color");
+modifyPageButton.addEventListener("click", () => {
+  const bgColor = prompt("Enter a color");
   const name = prompt("Enter your name");
-  description.style.backgroundColor = `${bcgColor}`;
-  modifyButton.style.backgroundColor = `${bcgColor}`;
+  description.style.backgroundColor = `${bgColor}`;
+  modifyPageButton.style.backgroundColor = `${bgColor}`;
+  modifyListButton.style.backgroundColor = `${bgColor}`;
   firstname.textContent = `${name}`;
   firstname.style.color = "white";
+  pinkText.forEach((element) => {
+    element.style.color = `${bgColor}`;
+  });
+  pinkBg.forEach((element) => {
+    element.style.backgroundColor = `${bgColor}`;
+  });
+  links.forEach((element) => {
+    element.style.color = `${bgColor}`;
+  });
 });
+
+modifyListButton.addEventListener("click", () => {
+  LiFrontDevTools[0].textContent = "VSCode";
+  LiFrontDevTools[1].textContent = "Github";
+  LiFrontDevTools[2].textContent = "Terminal";
+});
+
+modifyAddButton.addEventListener("click", () => {
+  let newLi = document.createElement("li");
+  newLi.textContent = `${inputBack.value}`;
+  ulBackTools.appendChild(newLi);
+})
