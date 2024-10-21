330ffd9f8885d59cdf47a2d741edc0a481240a6d
Hakkenheiner <remi.zicken@gmail.com>
2024-10-21 11:44:49 +0200

    finish Javascript step 7

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,18 +1,27 @@
 const avatar = document.querySelector('.avatar')
-const modifyButton = document.querySelector('.modify-button')
+const modifyButtonText = document.querySelector('#modify-button-text')
+const modifyButtonList = document.querySelector('#modify-button-list')
 const lien = document.querySelectorAll('a')
+const list = document.querySelectorAll('#front-dev-tools > li')
+const newList = ['VSCode', 'Github', 'Terminal']
 
 avatar.addEventListener('click', () => {
     avatar.src = './image/avatar.svg'
 })
 
-modifyButton.addEventListener('click', () => {
+modifyButtonText.addEventListener('click', () => {
     const color = prompt('Enter a color')
     const name = prompt('Enter yout name :')
     const firstName = document.querySelector('#firstname')
     firstName.textContent = name
     firstName.style.color = 'white'
     document.documentElement.style.setProperty('--lightWildColor', color);
+})
+
+modifyButtonList.addEventListener('click', () => {
+    list.forEach((element, index) => {
+        element.textContent = newList[index]
+    })
 })
 
 lien.forEach(element => {
