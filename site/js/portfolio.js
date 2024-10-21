e00bc2f553011c2f5cefea249a763947e91343d9
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-21 14:14:16 +0200

    Execute js code only when page is fully loaded.

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,49 +1,45 @@
+window.addEventListener("load", () => {
+    window.addEventListener("resize", () => {
+        const desktopElement = document.querySelector("figure.desktop");
+        if (desktopElement === null)
+            return;
+    
+        if (window.innerWidth <= 700) {
+            desktopElement.classList.add("desktop-hidden");
+        } else {
+            desktopElement.classList.remove("desktop-hidden");
+        }
+    });
+    
+    let toggleFlag = false;
+    const desktopImgElement = document.querySelector(".image-banner-first");
+    if (desktopImgElement !== null) {
+        desktopImgElement.addEventListener("click", (event) => {
+            toggleFlag = !toggleFlag;
+            if (toggleFlag) {
+                event.target.src = "image/avatar.svg";
+            } else {
+                event.target.src = "image/avatar-bis.png";
+            }
+        });
+    }
 
-window.addEventListener("resize", () => {
-    const desktopElement = document.querySelector("figure.desktop");
-    if (desktopElement === null)
-        return;
-
-    if (window.innerWidth <= 700) {
-        desktopElement.classList.add("desktop-hidden");
-    } else {
-        desktopElement.classList.remove("desktop-hidden");
+    const modifyTextAndColorElement = document.getElementById("modify-text-and-color");
+    if (modifyTextAndColorElement !== null) {
+        modifyTextAndColorElement.addEventListener("click", () => {
+            const firstNameElement = document.getElementById("firstname");
+            if (firstNameElement !== null) {
+                const color = prompt("Enter a color:"); 
+                const name = prompt("Enter your name:");
+                if (name !== null && color !== null) {
+                    const rootElement = document.querySelector(':root');
+                    if (rootElement !== null) {
+                        rootElement.style.setProperty("--lightWildColor", color);
+                    }
+                    firstNameElement.innerText = name;
+                    firstNameElement.style.color = "#FFFFFF";
+                }
+            }
+        });
     }
 });
-
-let toggleFlag = false;
-const desktopImgElement = document.querySelector(".image-banner-first");
-if (desktopImgElement !== null) {
-    desktopImgElement.addEventListener("click", (event) => {
-        toggleFlag = !toggleFlag;
-        if (toggleFlag) {
-            event.target.src = "image/avatar.svg";
-        } else {
-            event.target.src = "image/avatar-bis.png";
-        }
-    });
-}
-
-const modifyTextAndColorElement = document.getElementById("modify-text-and-color");
-if (modifyTextAndColorElement !== null) {
-    modifyTextAndColorElement.addEventListener("click", () => {
-        const firstNameElement = document.getElementById("firstname");
-        if (firstNameElement !== null) {
-            const color = prompt("Enter a color:"); 
-            const name = prompt("Enter your name:");
-            if (name !== null && color !== null) {
-                const rootElement = document.querySelector(':root');
-                if (rootElement !== null) {
-                    rootElement.style.setProperty("--lightWildColor", color);
-                }
-                firstNameElement.innerText = name;
-                firstNameElement.style.color = "#FFFFFF";
-            }
-        }
-    });
-}
-
-const linkElements = document.querySelectorAll("a");
-for (const linkElement of linkElements) {
-    linkElement.style.color = "#750ff7";
-}
