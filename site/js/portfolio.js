d9d380350702aa83b77809817e6d0aa162fe3707
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-21 12:03:47 +0200

    JS step 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,3 +10,30 @@ window.addEventListener("resize", () => {
         desktopElement.classList.remove("desktop-hidden");
     }
 });
+
+let toggle = false;
+const desktopImgElement = document.querySelector(".image-banner-first");
+if (desktopImgElement !== null) {
+    desktopImgElement.addEventListener("click", (event) => {
+        toggle = !toggle;
+        if (toggle) {
+            event.target.src = "image/avatar.svg";
+        } else {
+            event.target.src = "image/avatar-bis.png";
+        }
+    });
+}
+
+const modifyTextAndColorElement = document.getElementById("modify-text-and-color");
+if (modifyTextAndColorElement !== null) {
+    modifyTextAndColorElement.addEventListener("click", () => {
+        const firstNameElement = document.getElementById("firstname");
+        if (firstNameElement !== null) {
+            const name = prompt("Enter your name:");
+            if (name !== null) {
+                firstNameElement.innerText = name;
+                firstNameElement.style.color = "#FFFFFF";
+            }
+        }
+    });
+}
