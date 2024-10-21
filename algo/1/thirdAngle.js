f763a9f7e3d7401ee371de4ca68f610a2bb01ee6
JustineC31 <justine-cousin@hotmail.com>
2024-10-21 09:51:51 +0200

    Resolution algo 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,9 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  return 180 - (a + b);
 }
 
 module.exports = thirdAngle;
+thirdAngle (90, 30);
+thirdAngle (20, 80);
